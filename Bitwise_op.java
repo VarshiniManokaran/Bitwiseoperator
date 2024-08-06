38763401cdbc80061d417229d017f2df56f9e943
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 20:05:15 +0530

    Add files via upload

---
+++ Bitwise_op.java
@@ -0,0 +1,33 @@
+public class Bitwise_op {
+    public static void main(String[] args) {
+        int dd= 56;
+        int ee = 44;
+        System.out.println("The number is:"+ (dd & ee));
+
+
+
+        int vv=567;
+        int ff=786;
+        System.out.println("The number is:"+(ff | vv));
+
+
+        int ii = 998;
+        int nn = 876;
+        System.out.println("The number is :"+(ii ^ nn));
+
+
+        int cc = 987;
+        int tt = 876;
+        System.out.println("The number is:" +(cc >>> 1));
+
+        int ccc = 987;
+        int ttt = 876;
+        System.out.println("The number is:" +(ccc << 1));
+        System.out.println("The number is:" +(ccc >> 1));
+
+
+
+
+    }
+    
+}
